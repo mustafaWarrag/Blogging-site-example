92dfa82c92653fca3be4fa20fcf0e25352247e31
mustafaWarrag <mustafawarrag5@gmail.com>
2025-01-30 19:04:11 +0200

    fix: server issues

--- my-react-project/src/services/blogService.js
+++ my-react-project/src/services/blogService.js
@@ -1,17 +1,21 @@
 import axios from "axios"
 export default class blogService {
     static async fetchAllBlogs() {
-        return await axios.get("http://localhost:8080/api/v1/blogs");
+        //return await axios.get("http://localhost:8080/api/v1/blogs");
+        return await axios.get("https://blogging-site-example.onrender.com/api/v1/blogs");
     }
 
     static async fetchBlogById(id) {
-        return await axios.get(`http://localhost:8080/api/v1/blogs/id/${id}`)
+        //return await axios.get(`http://localhost:8080/api/v1/blogs/id/${id}`)
+        return await axios.get(`https://blogging-site-example.onrender.com/api/v1/blogs/id/${id}`)
     }
 
     static async fetchCategories() {
-        return await axios.get("http://localhost:8080/api/v1/blogs/tags");
+        //return await axios.get("http://localhost:8080/api/v1/blogs/tags");
+        return await axios.get("https://blogging-site-example.onrender.com/api/v1/blogs/tags");
     }
     static async fetchBlogsByCategories(tags, resultsPerPage, page) {
-        return await axios.get(`http://localhost:8080/api/v1/blogs?tags=${tags}&resultsPerPage=${resultsPerPage}&page=${page}`);
+        //return await axios.get(`http://localhost:8080/api/v1/blogs?tags=${tags}&resultsPerPage=${resultsPerPage}&page=${page}`);
+        return await axios.get(`https://blogging-site-example.onrender.com/api/v1/blogs?tags=${tags}&resultsPerPage=${resultsPerPage}&page=${page}`);
     }
 }
