d5fdfed4e223a317b0a8009103017c1bfab79007
mustafaWarrag <mustafawarrag5@gmail.com>
2025-02-11 16:21:39 +0200

    fix: removed a server route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,9 +4,10 @@ import userMiddleware from "../middleware/userMiddleware.js";
 
 let userRouter = express.Router();
 
-userRouter.route("/").get(userController.apiGetUsers)
+userRouter.route("/")//.get(userController.apiGetUsers)
                      .post(userController.apiCreateUser)
-                     .delete(userController.apiDeleteUser);
+                     //.delete(userController.apiDeleteUser);
+                     ;
 userRouter.route("/id/:id").get(userController.apiGetUserById);
 userRouter.route("/signin").post(userController.apiHandleSignin);
 userRouter.route("/profile").get(userMiddleware.tokenVerify, userController.apiGetUserById);
