70735c263d1faa36d49aed16438a4c9a21e6ee0a
mustafaWarrag <mustafawarrag5@gmail.com>
2025-02-11 17:30:52 +0200

    fix: configured for api hosting

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import blogRouter from "./routes/blog.routes.js";
 import userRouter from "./routes/user.routes.js";
 
+const renderIp = [process.env.IP_1, process.env.IP_2, process.env.IP_3]; //add local IP later
 
 configDotenv();
 
@@ -13,6 +14,13 @@ let app = express();
 app.use(express.json());
 //app.use(cors())
 app.use(cors({origin:"blogging-site-example.netlify.app"})); //only allow the website to fetch the data
+
+app.use((req, res, next) => {
+    if (!renderIp.includes(req.ip)) { //if IP address of the person who accessed the server isn't one of the whitelisted ones
+        res.status(403).json({message:"Access Denied"}); //then deny response
+    }
+    next();
+})
 app.use(morgan("dev"));
 
 app.get("/", (req,res) => {
